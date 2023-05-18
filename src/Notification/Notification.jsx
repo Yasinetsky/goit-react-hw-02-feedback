0551ce22a663fde37407169c41d4c134637316c3
Oleksii Yasynetskyi <ks7495395@gmail.com>
2023-05-18 22:31:52 +0300

    message fix

--- src/Notification/Notification.jsx
+++ src/Notification/Notification.jsx
@@ -1,22 +1,24 @@
 import style from './Notification.module.css';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import shortid from 'shortid';
+
 
 class Notification extends Component {
   static defaultProps = {
     message: ' ',
   };
-  static propTypes = {
-    Message: PropTypes.string,
-  };
+
   render() {
     return (
-      <h2 key={shortid.generate()} className={style.notification}>
+      <h2 className={style.notification}>
         {this.props.message}
       </h2>
     );
   }
 }
+
+Notification.propTypes = {
+  message: PropTypes.string.isRequired,
+};
 
 export default Notification;
